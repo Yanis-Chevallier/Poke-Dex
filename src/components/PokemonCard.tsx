361f0774ba1f6ac9357458dc3e00662113500b72
Yanis-Chevallier <yanischevallier2@gmail.com>
2025-01-29 12:09:33 +0100

    Quest 3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,25 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[1];
+
   return (
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>Pokemon de type plante</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
